9ae5503874f03782f070bcafe2d9647c03e534d5
badra9 <89783047+badra9@users.noreply.github.com>
2021-09-25 14:44:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button, Container } from "react-bootstrap";
+import "./App.css";
+
+import CurrencyConvert from "./components/currencyConvert/CurrencyConvert";
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyConvert />
+    </div>
+  );
+}
+
+export default App;
